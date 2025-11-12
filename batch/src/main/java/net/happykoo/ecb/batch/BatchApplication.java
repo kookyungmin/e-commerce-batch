e90df0897f387a27dd6b7c335e660490b16ee69e
gguri4549 <rudals4549@naver.com>
2025-11-12 10:46:17 +0900

    feat: batch 모듈 추가

---
+++ batch/src/main/java/net/happykoo/ecb/batch/BatchApplication.java
@@ -0,0 +1,12 @@
+package net.happykoo.ecb.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BatchApplication.class, args);
+  }
+}
